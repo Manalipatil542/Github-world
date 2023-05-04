ab5af020151ffe75fe80c46d15a0e92fdaadfdf2
Manali Patil <131386877+Manalipatil542@users.noreply.github.com>
2023-05-04 01:36:31 -0400

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from IPython.display import HTML
+HTML("""
+<video width="320" height="240" controls>
+  <source src= "https://github.com/Manalipatil542/Github-world/raw/main/video.mp4">
+</video>
+""")
+
+
+# 
